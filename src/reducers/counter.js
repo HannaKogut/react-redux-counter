af38279865998f8c0d600ec952f9f6b9aba6416d
HannaKogut <hanna.pol94@gmail.com>
2018-10-04 12:16:03 +0300

    finishing redux state for incriment-decrement app

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -8,10 +8,8 @@ export default (state = initialState, action) => {
     switch(action.type) {
         case INCREMENT:
             return { ...state, count: state.count + 1 };
-            break;
         case DECREMENT:
             return { ...state, count: state.count - 1 };
-            break;
         default:
             return state;
     }
