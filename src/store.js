21edea00017811fcbf7c6c58ea6714c905adb587
HannaKogut <hanna.pol94@gmail.com>
2018-10-04 11:01:41 +0300

    initializing store

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger({
+    collapsed: (getState, action, logEntry) => !logEntry.error
+});
+
+const middleware = applyMiddleware(logger);
+
+const store = createStore(
+    reducers,
+    compose(middleware, window.devToolsExtension ? window.devToolsExtension(): f => f)
+)
+
+export default store
