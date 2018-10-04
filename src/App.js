fffb95cb08b861b2d1004ebdb68e46f14b85b90b
HannaKogut <hanna.pol94@gmail.com>
2018-10-04 12:07:08 +0300

    implementing the EnhouncedCounter to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import EnhouncedCounter from './containers/EnhouncedCounter';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
         <h1 className="App-title">Simple counter</h1>
         </header>
+        <EnhouncedCounter />
       </div>
     );
   }
